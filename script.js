0cb97ff94dbb3348443e11fe6f816c9d7f235f06
Rishabh Tripathi <113877602+Rish-23072005@users.noreply.github.com>
2024-03-11 09:00:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-let result;
-        const myfun = () => {
-            let temp = document.getElementById('temp').value;
-            console.log(temp);
-            let tempselector = document.getElementById('select_temp');
-            let n = tempselector.options.selectedIndex;
-            let selectionis = tempselector.options[n].value;
-            if (selectionis == 'Celcius') {
-                result = ((temp * 1.8) + 32)
-                document.getElementById('myresult').innerHTML = `  ${result.toFixed(3)}  Fahrenheit`
-            }
-            else {
-                result = ((temp - 32) / 1.8);
-                document.getElementById('myresult').innerHTML = ` ${result.toFixed(3)} Celcius`
-            }
-        }
+let sidebar_navigation = document.getElementById('sidebar_navigation');
+
+let sidebar_navigation_bottom = document.getElementById('sidebar_buttom_menu');
+
+sidebar_navigation_bottom.addEventListener('click', function () {
+    if (sidebar_navigation.style.left == "" || sidebar_navigation.style.left == "-100%") {
+        sidebar_navigation.style.left = "0"
+        document.getElementById('sidebar_menu_icon').classList.add('fa-close');
+    }
+    else {
+        sidebar_navigation.style.left = "-100%"
+        document.getElementById('sidebar_menu_icon').classList.remove('fa-close');
+    }
+})
+
+
+let global_menu_buttom = document.getElementById('global_menu_button')
+let global_navigation = document.getElementById('global_menu_items')
+let global_close_buttom = document.getElementById('global_menu_close_button')
+
+global_menu_buttom.addEventListener('click', function () {
+    global_navigation.style.left = "0";
+})
+
+global_close_buttom.addEventListener('click', function () {
+    global_navigation.style.left = "-120%";
+})
